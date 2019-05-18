13834aa82cfecf93c6b5fd31d23bcfd6bfc1f3f7
Ken Youens-Clark <kyclark@gmail.com>
2019-05-18 10:54:12 -0700

    more ideas to solve

--- apples_and_bananas/solution.py
+++ apples_and_bananas/solution.py
@@ -55,21 +55,43 @@ def main():
         text = open(text).read()
 
     # Method 1: Iterate every character
-    new_text = []
-    for char in text:
-        if char in 'aeiou':
-            new_text.append(vowel)
-        elif char in 'AEIOU':
-            new_text.append(vowel.upper())
-        else:
-            new_text.append(char)
-    text = ''.join(new_text)
+    # new_text = []
+    # for char in text:
+    #     if char in 'aeiou':
+    #         new_text.append(vowel)
+    #     elif char in 'AEIOU':
+    #         new_text.append(vowel.upper())
+    #     else:
+    #         new_text.append(char)
+    # text = ''.join(new_text)
 
     # Method 2: str.replace
     # for v in 'aeiou':
     #     text = text.replace(v, vowel).replace(v.upper(), vowel.upper())
 
-    # Method 3: Regular expressions
+
+    # Method 3: Use a list comprehension
+    # new_text = [
+    #     vowel if c in 'aeiou' else vowel.upper() if c in 'AEIOU' else c
+    #     for c in text
+    # ]
+    # text = ''.join(new_text)
+
+    # Method 4: Define a function, use list comprehension
+    def new_char(c):
+        return vowel if c in 'aeiou' else vowel.upper() if c in 'AEIOU' else c
+    # text = ''.join([new_char(c) for c in text])
+
+    # Method 5: Use a `map` to iterate with a `lambda`
+    # text = ''.join(
+    #     map(
+    #         lambda c: vowel if c in 'aeiou' else vowel.upper()
+    #         if c in 'AEIOU' else c, text))
+
+    # Method 6: `map` with the function
+    text = ''.join(map(new_char, text))
+
+    # Method 7: Regular expressions
     # text = re.sub('[aeiou]', vowel, text)
     # text = re.sub('[AEIOU]', vowel.upper(), text)
 
