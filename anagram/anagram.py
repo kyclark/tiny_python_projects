9c476d4cc7c4752aceb77df0719a08b61ee42bac
Ken Youens-Clark <kyclark@gmail.com>
2019-05-19 12:51:46 -0700

    working

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -10,8 +10,8 @@ import logging
 import os
 import re
 import sys
-from collections import Counter
-from itertools import combinations
+from collections import defaultdict, Counter
+from itertools import combinations, product
 
 
 # --------------------------------------------------
@@ -30,16 +30,14 @@ def get_args():
                         type=str,
                         default='/usr/share/dict/words')
 
-    parser.add_argument(
-        '-n',
-        '--num_combos',
-        help='Number of words combination to test',
-        metavar='int',
-        type=int,
-        default=1)
+    parser.add_argument('-n',
+                        '--num_combos',
+                        help='Number of words combination to test',
+                        metavar='int',
+                        type=int,
+                        default=1)
 
-    parser.add_argument(
-        '-d', '--debug', help='Debug', action='store_true')
+    parser.add_argument('-d', '--debug', help='Debug', action='store_true')
 
     return parser.parse_args()
 
@@ -72,24 +70,40 @@ def main():
         filemode='w',
         level=logging.DEBUG if args.debug else logging.CRITICAL)
 
-    words = set()
+    words = defaultdict(set)
     for line in open(word_list):
         for word in line.split():
-            words.add(re.sub('[^a-z0-9]', '', word.lower()))
-
-    # p = filter(lambda w: w != text and Counter(w) == counts, words)
+            clean = re.sub('[^a-z0-9]', '', word.lower())
+            if len(clean) == 1 and clean not in 'ai':
+                continue
+            words[len(clean)].add(clean)
 
     text_len = len(text)
     counts = Counter(text)
     anagrams = []
+    lengths = list(words.keys())
     for i in range(1, args.num_combos + 1):
-        for t in map(lambda x: ' '.join(x), combinations(words, i)):
-            logging.debug(t)
-            if t != text and Counter(t.replace(' ', '')) == counts:
-                anagrams.append(t)
+        key_combos = list(
+            filter(lambda t: sum(t) == text_len, combinations(lengths, i)))
+
+        for keys in key_combos:
+            logging.debug('Searching keys {}'.format(keys))
+            word_combos = list(product(*list(map(lambda k: words[k], keys))))
+            anagrams.extend(
+                filter(
+                    lambda x: x != text,
+                    map(
+                        lambda t: ' '.join(t),
+                        filter(lambda t: Counter(''.join(t)) == counts,
+                               word_combos))))
+            logging.debug('# anagrams = {}'.format(len(anagrams)))
+
+    logging.debug('Finished searching')
 
     if anagrams:
-        print('{} = {}'.format(text, ', '.join(anagrams)))
+        print('{} = '.format(text))
+        for i, t in enumerate(anagrams, 1):
+            print('{:4}. {}'.format(i, t))
     else:
         print('No anagrams for "{}".'.format(text))
 
