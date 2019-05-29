cdc4a0d44e4bfbadca23b6a79938a737774a2f49
Ken Youens-Clark <kyclark@gmail.com>
2019-05-29 15:20:19 -0700

    reader

---
+++ movie_reader/README.md
@@ -0,0 +1,23 @@
+# Movie Reader
+
+Write a Python program called `movie_reader.py` that takes a single positional argument that is a bit of text or the name of an input file. The output will be dynamic, so I cannot write a test for how the program should behave, nor can I include a bit of text that shows you how it should work. Your program should print the input text character-by-character and then pause .5 seconds for ending punctuation like `.`, `!` or `?`, .2 seconds for a pause like `,` `:`, or `;`, and .05 seconds for anything else.
+
+````
+$ ./movie_reader.py
+usage: movie_reader.py [-h] str
+movie_reader.py: error: the following arguments are required: str
+$ ./movie_reader.py -h
+usage: movie_reader.py [-h] str
+
+Movie Reader
+
+positional arguments:
+  str         Input text or file
+
+optional arguments:
+  -h, --help  show this help message and exit
+$ ./movie_reader.py 'Foo, bar!'
+Foo, bar!
+$ ./movie_reader.py ../inputs/fox.txt
+The quick brown fox jumps over the lazy dog.
+````
---
+++ movie_reader/solution.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Author : Ken Youens-Clark <kyclark@gmail.com>
+Date   : 2019-05-29
+Purpose: Movie Reader
+"""
+
+import argparse
+import os
+import sys
+import time
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Movie Reader',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text',
+                        metavar='str',
+                        help='Input text or file')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    text = args.text
+
+    if os.path.isfile(text):
+        text = open(text).read()
+
+    for line in text.splitlines():
+        for char in line:
+            print(char, end='')
+            time.sleep(.5 if char in '.!?\n' else .2 if char in ',:;' else .05)
+            sys.stdout.flush()
+
+        print()
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
