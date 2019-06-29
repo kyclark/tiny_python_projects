55aa9fc9f9c20ae35119dbfd59001a1ccaeff645
Ken Youens-Clark <kyclark@gmail.com>
2019-06-28 17:00:39 -0700

    more specs, better program and tests

--- soundex_rhymer/test.py
+++ soundex_rhymer/test.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 """tests for rhymer.py"""
 
-import re
+import os
 import random
+import re
 from subprocess import getstatusoutput, getoutput
 
 prg = './rhymer.py'
+
+
+# --------------------------------------------------
+def test_exists():
+    """exists"""
+
+    assert os.path.isfile(prg)
 
 
 # --------------------------------------------------
@@ -19,13 +27,37 @@ def test_usage():
 
 
 # --------------------------------------------------
-def test_play01():
-    out = getoutput('{} listen'.format(prg)).splitlines()
-    assert len(out) == 161
-    assert ('wisdom' in out)
+def test_no_vowel():
+    """test"""
+
+    word = ''.join(random.sample('bcdtprhmntghqw', k=5))
+    rv, out = getstatusoutput('{} {}'.format(prg, word))
+    assert rv != 0
+    expected = 'word "{}" must contain at least one vowel'.format(word)
+    assert re.search(expected, out)
 
 
 # --------------------------------------------------
-def test_play02():
+def test_01():
+    """test"""
+
+    out = getoutput('{} -s listen'.format(prg)).splitlines()
+    assert len(out) == 196
+    assert 'wisdom' in out
+
+
+# --------------------------------------------------
+def test_02():
+    """test"""
+
+    out = getoutput('{} listen'.format(prg)).splitlines()
+    assert len(out) == 37
+    assert 'lighten' in out
+
+
+# --------------------------------------------------
+def test_03():
+    """test"""
+
     out = getoutput('{} orange'.format(prg))
-    assert len(out.splitlines()) == 150
+    assert len(out.splitlines()) == 7
