9208cab3f36af6e31da9dbca0cdc6395731682cc
Ken Youens-Clark <kyclark@gmail.com>
2019-07-13 14:05:01 -0600

    some tests

--- pareto/test.py
+++ pareto/test.py
@@ -27,6 +27,28 @@ def test_usage():
         assert re.match("usage", out, re.IGNORECASE)
 
 
+# --------------------------------------------------
+def test_runs1():
+    """Runs"""
+
+    expected = """
+  1: 2115 iterations
+  2: 2058 iterations
+  3: 1330 iterations
+  4: 2139 iterations
+  5: 2294 iterations
+  6: 1226 iterations
+  7: 1663 iterations
+  8: 1603 iterations
+  9: 1088 iterations
+ 10: 2008 iterations
+Average = 1,752 iterations
+    """.strip()
+
+    rv, out = getstatusoutput('{} -s 1'.format(prg))
+    assert rv == 0
+    assert out.strip() == expected
+
 # # --------------------------------------------------
 # def test_bad_int():
 #     """Bad integer value"""
