24fe8f9a10bd948804865cde54a1ded4e959660d
Ken Youens-Clark <kyclark@gmail.com>
2019-08-07 11:29:46 -0700

    solution.py

--- wod/solution.py
+++ wod/solution.py
@@ -3,10 +3,10 @@
 
 import argparse
 import csv
-import os
+import io
 import random
+from itertools import starmap
 from tabulate import tabulate
-from dire import die
 
 
 # --------------------------------------------------
@@ -49,7 +49,6 @@ def get_args():
 # --------------------------------------------------
 def read_csv(fh):
     """Read the CSV input"""
-
     exercises = []
 
     for row in csv.DictReader(fh, delimiter=','):
@@ -61,22 +60,28 @@ def read_csv(fh):
 
 
 # --------------------------------------------------
+def test_read_csv():
+    """Test read_csv"""
+
+    text = io.StringIO('exercise,reps\nfoo,10-20\nbar,30-40')
+    assert read_csv(text) == [('foo', 10, 20), ('bar', 30, 40)]
+
+
+# --------------------------------------------------
 def main():
     """Make a jazz noise here"""
 
     args = get_args()
     random.seed(args.seed)
     exercises = read_csv(args.file)
-    table = []
 
-    for name, low, high in random.sample(exercises, k=args.num_exercises):
+    def mk_exercise(name, low, high):
         if args.easy:
-            low = int(low / 2)
-            high = int(high / 2)
+            low, high = int(low / 2), int(high / 2)
+        return (name, random.randint(low, high))
 
-        table.append((name, random.randint(low, high)))
-
-    print(tabulate(table, headers=('Exercise', 'Reps')))
+    wod = starmap(mk_exercise, random.sample(exercises, k=args.num_exercises))
+    print(tabulate(wod, headers=('Exercise', 'Reps')))
 
 
 # --------------------------------------------------
