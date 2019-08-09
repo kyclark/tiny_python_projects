7835392bf50686bad0bc93782054a60b426ae02c
Ken Youens-Clark <kyclark@gmail.com>
2019-08-09 08:29:48 -0700

    solution.py

--- twelve_days_of_christmas/solution.py
+++ twelve_days_of_christmas/solution.py
@@ -20,7 +20,7 @@ def get_args():
                         default='')
 
     parser.add_argument('-n',
-                        '--number_days',
+                        '--num',
                         help='Number of days to sing',
                         metavar='int',
                         type=int,
@@ -34,50 +34,41 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    out_file = args.outfile
-    num_days = args.number_days
-    out_fh = open(out_file, 'wt') if out_file else sys.stdout
+    num_days = args.num
+    out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
 
-    days = {
-        12: 'Twelve drummers drumming',
-        11: 'Eleven pipers piping',
-        10: 'Ten lords a leaping',
-        9: 'Nine ladies dancing',
-        8: 'Eight maids a milking',
-        7: 'Seven swans a swimming',
-        6: 'Six geese a laying',
-        5: 'Five gold rings',
-        4: 'Four calling birds',
-        3: 'Three French hens',
-        2: 'Two turtle doves',
-        1: 'a partridge in a pear tree',
-    }
+    days = [
+        'A partridge in a pear tree.',
+        'Two turtle doves',
+        'Three French hens',
+        'Four calling birds',
+        'Five gold rings',
+        'Six geese a laying',
+        'Seven swans a swimming',
+        'Eight maids a milking',
+        'Nine ladies dancing',
+        'Ten lords a leaping',
+        'Eleven pipers piping',
+        'Twelve drummers drumming',
+    ]
 
-    ordinal = {
-        12: 'twelfth', 11: 'eleven', 10: 'tenth',
-        9: 'ninth', 8: 'eighth', 7: 'seventh',
-        6: 'sixth', 5: 'fifth', 4: 'fourth',
-        3: 'third', 2: 'second', 1: 'first',
-    }
-
-    if not num_days in days:
+    if not num_days in range(1, len(days) + 1):
         print('Cannot sing "{}" days'.format(num_days))
         sys.exit(1)
 
-    for i in range(1, num_days + 1):
-        first = 'On the {} day of Christmas,\nMy true love gave to me,'
-        out_fh.write(first.format(ordinal[i]) + '\n')
-        for j in reversed(range(1, i + 1)):
-            if j == 1:
-                if i == 1:
-                    out_fh.write('{}.\n'.format(days[j].title()))
-                else:
-                    out_fh.write('And {}.\n'.format(days[j]))
-            else:
-                out_fh.write('{},\n'.format(days[j]))
+    ordinal = [
+        'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh',
+        'eighth', 'ninth', 'tenth', 'eleven', 'twelfth'
+    ]
 
-        if i < max(days.keys()):
-            out_fh.write('\n')
+    first = 'On the {} day of Christmas,\nMy true love gave to me,'
+    for i in range(1, num_days + 1):
+        out_fh.write(first.format(ordinal[i - 1]) + '\n')
+        lines = list(reversed(days[:i]))
+        if len(lines) > 1:
+            lines[-1] = 'And ' + lines[-1].lower()
+        _ = out_fh.write(',\n'.join(lines) +
+                         ('\n\n' if i < num_days else '\n'))
 
 
 # --------------------------------------------------
