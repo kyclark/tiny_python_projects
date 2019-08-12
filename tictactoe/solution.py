4c4c4d87a58ae028aff4f362bd261cb46ade176e
Ken Youens-Clark <kyclark@gmail.com>
2019-08-12 08:54:31 -0700

    solution.py

--- tictactoe/solution.py
+++ tictactoe/solution.py
@@ -1,29 +1,98 @@
 #!/usr/bin/env python3
+"""Tic-Tac-Toe"""
 
-import os
+import argparse
 import re
 import sys
 
 
 # --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Tic-Tac-Toe board',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-s',
+                        '--state',
+                        help='Board state',
+                        metavar='str',
+                        type=str,
+                        default='.........')
+
+    parser.add_argument('-p',
+                        '--player',
+                        help='Player',
+                        metavar='str',
+                        type=str,
+                        default=None)
+
+    parser.add_argument('-c',
+                        '--cell',
+                        help='Cell to apply -p',
+                        metavar='int',
+                        type=int,
+                        default=None)
+
+    args = parser.parse_args()
+
+    if args.player and args.player not in 'XO':
+        parser.error('Invalid player "{}", must be X or O'.format(args.player))
+
+    if args.cell is not None and not 1 <= args.cell <= 9:
+        parser.error('Invalid cell "{}", must be 1-9'.format(args.cell))
+
+    if any([args.player, args.cell]) and not all([args.player, args.cell]):
+        parser.error('Must provide both --player and --cell')
+
+    if not re.search('^[.XO]{9}$', args.state):
+        parser.error(
+            'Invalid state "{}", must be 9 characters of only -, X, O'.format(
+                args.state))
+
+    return args
+
+
+# --------------------------------------------------
 def main():
-    args = sys.argv[1:]
+    """Make a jazz noise here"""
 
-    if len(args) != 1:
-        print('Usage: {} STATE'.format(os.path.basename(sys.argv[0])))
-        sys.exit(1)
+    args = get_args()
+    state = args.state
+    player = args.player
+    cell = args.cell
 
-    state = args[0]
+    bar = '-------------'
+    cells_tmpl = '| {} | {} | {} |'
 
-    if not re.search('^[.XO]{9}$', state):
-        print('State "{}" must be 9 characters of only ., X, O'.format(state),
-              file=sys.stderr)
-        sys.exit(1)
+    cells = []
+    for i, char in enumerate(state, start=1):
+        cells.append(str(i) if char == '.' else char)
+
+    if player and cell:
+        if cells[cell - 1] not in 'XO':
+            cells[cell - 1] = player
+        else:
+            print('Cell {} already taken'.format(cell))
+            sys.exit(1)
+
+    print('\n'.join([
+        bar,
+        cells_tmpl.format(cells[0], cells[1], cells[2]), bar,
+        cells_tmpl.format(cells[3], cells[4], cells[5]), bar,
+        cells_tmpl.format(cells[6], cells[7], cells[8]), bar
+    ]))
+
+    winner = get_winner(state)
+    print(f'{winner} won!' if winner else 'No winner.')
+
+
+# --------------------------------------------------
+def get_winner(state):
+    """Return winning player if any"""
 
     winning = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
                [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-
-    winner = 'No winner'
 
     # for player in ['X', 'O']:
     #     for combo in winning:
@@ -53,8 +122,7 @@ def main():
         for i, j, k in winning:
             combo = [state[i], state[j], state[k]]
             if combo == [player, player, player]:
-                winner = '{} has won'.format(player)
-                break
+                return (player)
 
     # for combo in winning:
     #     group = list(map(lambda i: state[i], combo))
@@ -62,8 +130,6 @@ def main():
     #         if all(x == player for x in group):
     #             winner = player
     #             break
-
-    print(winner)
 
 
 # --------------------------------------------------
