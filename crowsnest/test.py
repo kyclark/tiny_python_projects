d3bf3cf5a1016a9a13fc59555225e0856affc612
Ken Youens-Clark <kyclark@gmail.com>
2019-10-05 06:24:10 -0700

    article -> crowsnest

---
+++ crowsnest/test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""tests for crowsnest.py"""
+
+import os
+from subprocess import getstatusoutput, getoutput
+
+prg = './crowsnest.py'
+consonant_words = [
+    'brigatine', 'clipper', 'dreadnought', 'frigate', 'galleon', 'haddock',
+    'junk', 'ketch', 'longboat', 'mullet', 'narwhal', 'porpoise', 'quay',
+    'regatta', 'submarine', 'tanker', 'vessel', 'whale', 'xebec', 'yatch',
+    'zebrafish'
+]
+vowel_words = ['aviso', 'eel', 'iceberg', 'octopus', 'upbound']
+template = 'Ahoy, Captain, {} {} off the larboard bow!'
+
+
+# --------------------------------------------------
+def test_exists():
+    """exists"""
+
+    assert os.path.isfile(prg)
+
+
+# --------------------------------------------------
+def test_usage():
+    """usage"""
+
+    for flag in ['-h', '--help']:
+        rv, out = getstatusoutput('{} {}'.format(prg, flag))
+        assert rv == 0
+        assert out.lower().startswith('usage')
+
+
+# --------------------------------------------------
+def test_consonant():
+    """brigatine -> a brigatine"""
+
+    for word in consonant_words:
+        out = getoutput('{} {}'.format(prg, word))
+        assert out.strip() == template.format('a', word)
+
+
+# --------------------------------------------------
+def test_consonant_upper():
+    """brigatine -> a Brigatine"""
+
+    for word in consonant_words:
+        out = getoutput('{} {}'.format(prg, word.title()))
+        assert out.strip() == template.format('a', word.title())
+
+# --------------------------------------------------
+def test_vowel():
+    """octopus -> an octopus"""
+
+    for word in vowel_words:
+        out = getoutput('{} {}'.format(prg, word))
+        assert out.strip() == template.format('an', word)
+
+# --------------------------------------------------
+def test_vowel_upper():
+    """octopus -> an Octopus"""
+
+    for word in vowel_words:
+        out = getoutput('{} {}'.format(prg, word.upper()))
+        assert out.strip() == template.format('an', word.upper())
