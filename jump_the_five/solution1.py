4560d486c4f60c4668eba630c43506c1cc3a990b
Ken Youens-Clark <kyclark@gmail.com>
2019-12-16 21:44:41 -0700

    no comment

--- jump_the_five/solution1.py
+++ jump_the_five/solution1.py
@@ -25,7 +25,6 @@ def main():
     jumper = {'1': '9', '2': '8', '3': '7', '4': '6', '5': '0',
               '6': '4', '7': '3', '8': '2', '9': '1', '0': '5'}
 
-    # Method 1: for loop with print
     for char in args.text:
         print(jumper.get(char, char), end='')
     print()
