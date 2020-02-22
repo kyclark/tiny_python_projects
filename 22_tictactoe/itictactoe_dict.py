3a13338a81d5aae1ec0ef5fc65a433b21e8c8ec2
Ken Youens-Clark <kyclark@gmail.com>
2020-02-22 08:20:13 -0700

    testing typeddict/namedtuple

--- 22_tictactoe/itictactoe_dict.py
+++ 22_tictactoe/itictactoe_dict.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-""" Interactive Tic-Tac-Toe using NamedTuple """
+""" Interactive Tic-Tac-Toe using TypedDict """
 
-from typing import List, NamedTuple, Optional
+from typing import List, Optional, TypedDict
 
 
-class State(NamedTuple):
-    board: List[str]
+class State(TypedDict):
+    board: str
     player: str
     quit: bool
     draw: bool
@@ -17,7 +17,7 @@ class State(NamedTuple):
 def main() -> None:
     """Make a jazz noise here"""
 
-    state = State(board=list('.' * 9),
+    state = State(board='.' * 9,
                   player='X',
                   quit=False,
                   draw=False,
@@ -26,20 +26,21 @@ def main() -> None:
 
     while True:
         print("\033[H\033[J")
-        print(format_board(state.board))
+        print(format_board(state['board']))
 
-        if state.error:
-            print(state.error)
+        if state['error']:
+            print(state['error'])
 
         state = get_move(state)
+        print(state)
 
-        if state.quit:
+        if state['quit']:
             print('You lose, loser!')
             break
-        elif state.winner:
-            print(f'{state.winner} has won!')
+        elif state['winner']:
+            print(f"{state['winner']} has won!")
             break
-        elif state.draw:
+        elif state['draw']:
             print('No winner.')
             break
 
@@ -48,30 +49,37 @@ def main() -> None:
 def get_move(state: State) -> State:
     """Get the player's move"""
 
-    player = state.player
+    player = state['player']
     cell = input(f'Player {player}, what is your move? [q to quit]: ')
 
     if cell == 'q':
-        return state._replace(quit=True)
+        state['quit'] = True
+        return state
 
     if not (cell.isdigit() and int(cell) in range(1, 10)):
-        return state._replace(error=f'Invalid cell "{cell}", please use 1-9')
+        state['error'] = f'Invalid cell "{cell}", please use 1-9'
+        return state
 
     cell_num = int(cell)
-    if state.board[cell_num - 1] in 'XO':
-        return state._replace(error=f'Cell "{cell}" already taken')
+    if state['board'][cell_num - 1] in 'XO':
+        state['error'] = f'Cell "{cell}" already taken'
+        return state
 
-    board = state.board
+    board = list(state['board'])
     board[cell_num - 1] = player
-    return state._replace(board=board,
-                          player='O' if player == 'X' else 'X',
-                          winner=find_winner(board),
-                          draw='.' not in board,
-                          error=None)
+
+    return State(
+        board=''.join(board),
+        player='O' if player == 'X' else 'X',
+        winner=find_winner(board),
+        draw='.' not in board,
+        error=None,
+        quit=False,
+    )
 
 
 # --------------------------------------------------
-def format_board(board: List[str]) -> str:
+def format_board(board: str) -> str:
     """Format the board"""
 
     cells = [str(i) if c == '.' else c for i, c in enumerate(board, 1)]
