797dc9244446c43b0069c51ac2db1c827226f281
Ken Youens-Clark <kyclark@gmail.com>
2020-02-22 12:21:29 -0700

    renumbering dirs

--- 16_kentucky_friar/mk-outs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-PRG="./friar.py"
-
-for FILE in tests/*.txt; do
-    $PRG $FILE > $FILE.out
-done
--- 20_wod/using_csv2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import csv
-from pprint import pprint
-
-with open('exercises.csv') as fh:
-    reader = csv.DictReader(fh, delimiter=',')
-    records = list(reader)
-    pprint(records)
