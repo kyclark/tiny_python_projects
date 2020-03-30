dcfaeea617123d93ed42b443dd56a0fcf13e1c05
Ken Youens-Clark <kyclark@gmail.com>
2020-03-30 12:00:55 -0700

    transpose table, more flexible

--- 18_gematria/asciitbl.py
+++ 18_gematria/asciitbl.py
@@ -1,20 +1,70 @@
 #!/usr/bin/env python3
-"""Print ASCII table"""
+""" Make ASCII table """
 
 import argparse
-import os
-import sys
-import string
 from itertools import zip_longest
 from tabulate import tabulate
+import pandas as pd
+from pprint import pprint
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Make ASCII table',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-c',
+                        '--cols',
+                        help='Number of columns',
+                        metavar='int',
+                        type=int,
+                        default=8)
+
+    parser.add_argument('-l',
+                        '--lower',
+                        help='Lower chr value',
+                        metavar='int',
+                        type=int,
+                        default=0)
+
+    parser.add_argument('-u',
+                        '--upper',
+                        help='Upper chr value',
+                        metavar='int',
+                        type=int,
+                        default=128)
+
+    args = parser.parse_args()
+
+    if args.lower < 0:
+        parser.error(f'--lower "{args.lower}" must be >= 0')
+
+    if args.upper > 128:
+        parser.error(f'--upper "{args.upper}" must be <= 128')
+
+    if args.lower > args.upper:
+        args.lower, args.upper = args.upper, args.lower
+
+    return args
 
 
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
 
-    for chunk in chunker(range(128), 8):
-        print('  '.join(map(cell, chunk)))
+    args = get_args()
+    lower = args.lower
+    upper = args.upper
+    num_cells = args.upper - args.lower
+    num_rows = round(num_cells / args.cols)
+    cells = list(chunker(list(map(cell, range(lower, upper))), num_rows))
+    df = pd.DataFrame(cells)
+
+    for i, row in df.T.iterrows():
+        print('  '.join(map(lambda v: v or ' ' * 5, row)))
 
 
 # --------------------------------------------------
