62e49fe33bb2e2765b7b6b3b1b86768cb32008ab
Ken Youens-Clark <kyclark@gmail.com>
2020-05-20 11:28:39 -0700

    better var names

--- 11_bottles_of_beer/solution.py
+++ 11_bottles_of_beer/solution.py
@@ -55,15 +55,15 @@ def verse(bottle):
 def test_verse():
     """Test verse"""
 
-    one = verse(1)
-    assert one == '\n'.join([
+    last_verse = verse(1)
+    assert last_verse == '\n'.join([
         '1 bottle of beer on the wall,', '1 bottle of beer,',
         'Take one down, pass it around,',
         'No more bottles of beer on the wall!'
     ])
 
-    two = verse(2)
-    assert two == '\n'.join([
+    two_bottles = verse(2)
+    assert two_bottles == '\n'.join([
         '2 bottles of beer on the wall,', '2 bottles of beer,',
         'Take one down, pass it around,', '1 bottle of beer on the wall!'
     ])
