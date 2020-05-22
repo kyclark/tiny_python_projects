af1650b5212a744cd49fb817acd540cf3f1678b2
Ken Youens-Clark <kyclark@gmail.com>
2020-05-22 09:36:18 -0700

    another version

---
+++ 15_kentucky_friar/solution2_re_compile.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""Kentucky Friar"""
+
+import argparse
+import os
+import re
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Southern fry text',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text', metavar='text', help='Input text or file')
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    splitter = re.compile(r'(\W+)')
+
+    for line in args.text.splitlines():
+        print(''.join(map(fry, splitter(line.rstrip()))))
+
+
+# --------------------------------------------------
+def fry(word):
+    """Drop the `g` from `-ing` words, change `you` to `y'all`"""
+
+    ing_word = re.search('(.+)ing$', word)
+    you = re.match('([Yy])ou$', word)
+
+    if ing_word:
+        prefix = ing_word.group(1)
+        if re.search('[aeiouy]', prefix, re.IGNORECASE):
+            return prefix + "in'"
+    elif you:
+        return you.group(1) + "'all"
+
+    return word
+
+
+# --------------------------------------------------
+def test_fry():
+    """Test fry"""
+
+    assert fry('you') == "y'all"
+    assert fry('You') == "Y'all"
+    assert fry('your') == 'your'
+    assert fry('fishing') == "fishin'"
+    assert fry('Aching') == "Achin'"
+    assert fry('swing') == "swing"
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
