b652e1a69bd0514e4bd951b58220b7e375546fbc
Ken Youens-Clark <kyclark@gmail.com>
2020-06-25 10:38:42 -0700

    fix

--- all_test.sh
+++ all_test.sh
@@ -5,9 +5,8 @@ find . -name all_test.sh > "$TESTS"
 
 while read -r TEST; do
     DIR=$(dirname "$TEST")
-    echo "\n\n==> $DIR <==\n\n"
-    cd "$DIR"
-    ./all_test.sh
+    echo -e "\n\n==> $DIR <==\n\n"
+    (cd "$DIR" && ./all_test.sh)
 done < "$TESTS"
 
 rm "$TESTS"
