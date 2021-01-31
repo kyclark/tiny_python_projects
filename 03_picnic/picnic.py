7f8c553c19cca55e06d736eb5fd15a383229b842
Petra Power <petra.powered@gmail.com>
2021-01-30 16:58:55 -0800

    adding new file for picnic program, program accepts args, displays correct help

---
+++ 03_picnic/picnic.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Author : petra <petra@localhost>
+Date   : 2021-01-30
+Purpose: Picnic game
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Picnic game',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('str',
+                        metavar='str',
+                        nargs='+',           # one or more expected
+                        type=str,
+                        help='Item(s) to bring')
+
+    parser.add_argument('-s',
+                        '--sorted',
+                        help='Sort the items',
+                        action='store_true')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    str_arg = args.arg
+    int_arg = args.int
+    file_arg = args.file
+    flag_arg = args.on
+    pos_arg = args.positional
+
+    print(f'str_arg = "{str_arg}"')
+    print(f'int_arg = "{int_arg}"')
+    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
+    print(f'flag_arg = "{flag_arg}"')
+    print(f'positional = "{pos_arg}"')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
