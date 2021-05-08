90d47831f7cdc3869baf076fd378aa1f23ed910e
Aman Makkar <67231395+Aman-Makkar@users.noreply.github.com>
2021-05-08 17:11:42 +0100

    First and second chapter files

---
+++ 01_hello/hello.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Author: Aman Makkar
+# Purpose: Say hello
+"""
+
+import argparse
+
+
+# --------------------------------------------
+def get_args():
+    """Get the command-line arguments"""
+    parser = argparse.ArgumentParser(description='Say Hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+# --------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+# --------------------------------------------
+if __name__ == '__main__':
+    main()
