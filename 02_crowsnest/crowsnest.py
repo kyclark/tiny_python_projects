b1df7924484bb62f149626fe107c90be0eda7d78
mattdube <xero.luck@gmail.com>
2021-05-18 19:57:59 -0500

    moved article logic to function

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -25,13 +25,24 @@ def print_object(object_seen):
     """Print object seen
        First check for vowel or consonant
     """
-    vowels = ('a', 'e', 'i', 'o', 'u')
 
-    if object_seen[0].lower() in vowels:
-        print("Ahoy, Captain, an " + object_seen + " off the larboard bow!")
-    else:
-        print("Ahoy, Captain, a " + object_seen + " off the larboard bow!")
+    article = get_article(object_seen)
+    print(f'Ahoy, Captain, {article} {object_seen} off the larboard bow!')
+    #vowels = ('a', 'e', 'i', 'o', 'u')
 
+    #if object_seen[0].lower() in vowels:
+    #    print("Ahoy, Captain, an " + object_seen + " off the larboard bow!")
+    #else:
+    #    print("Ahoy, Captain, a " + object_seen + " off the larboard bow!")
+
+
+def get_article(word):
+    article = ''
+    if word[0].lower() in 'aeiou':
+        article = 'an'
+    else: 
+        article = 'a'
+    return article
 
 # --------------------------------------------------
 def main():
