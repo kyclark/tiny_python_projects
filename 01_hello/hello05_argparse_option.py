995645de4bc35fdee06df4b71d2a0a7a5f06a2a7
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:19:21 +0530

    Format Code

--- 01_hello/hello05_argparse_option.py
+++ 01_hello/hello05_argparse_option.py
@@ -3,8 +3,9 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('-n', '--name', metavar='name',
-                    default='World', help='Name to greet')
+parser = argparse.ArgumentParser(description="Say hello")
+parser.add_argument(
+    "-n", "--name", metavar="name", default="World", help="Name to greet"
+)
 args = parser.parse_args()
-print('Hello, ' + args.name + '!')
+print("Hello, " + args.name + "!")
