dbd1ce7cb77c39704b04d28071a01ef667d4a7b4
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:20:36 +0530

    Format Code

--- 01_hello/hello07_get_args.py
+++ 01_hello/hello07_get_args.py
@@ -3,15 +3,19 @@
 
 import argparse
 
+
 def get_args():
-    parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name',
-                        default='World', help='Name to greet')
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument(
+        "-n", "--name", metavar="name", default="World", help="Name to greet"
+    )
     return parser.parse_args()
+
 
 def main():
     args = get_args()
-    print('Hello, ' + args.name + '!')
+    print("Hello, " + args.name + "!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
