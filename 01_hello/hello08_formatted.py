8224d32b5b8965f7ba27f3bdf97f51006b6c121d
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:21:16 +0530

    Format Code

--- 01_hello/hello08_formatted.py
+++ 01_hello/hello08_formatted.py
@@ -11,8 +11,8 @@ import argparse
 def get_args():
     """Get the command-line arguments"""
 
-    parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', default='World', help='Name to greet')
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument("-n", "--name", default="World", help="Name to greet")
     return parser.parse_args()
 
 
@@ -21,9 +21,9 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    print('Hello, ' + args.name + '!')
+    print("Hello, " + args.name + "!")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
