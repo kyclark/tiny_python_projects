d9b24d81103ef575f6f5a76a7d2a05a5d8eb9d34
hhaemin <81980221+hhaemin@users.noreply.github.com>
2022-12-20 21:50:25 +0900

    문자열 처리

---
+++ 02_crowsnest/crowsnest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Author : haemin <haemin@localhost>
+Date   : 2022-12-06
+Purpose: Crow's Nest
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description = "Crow's Nest -- choose the correct article",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('word', metavar = 'word', help='A word')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    word = args.word
+    article = 'an' if word[0].lower() in 'aeiou' else 'a'
+    
+    print(f'Ahoy, Captain, {article} {word} off the larboard bow!')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
