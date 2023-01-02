e070255edd8e0243ea91fe9e1458557c6a2ed953
theFippo <schmid.jeffrey@gmail.com>
2023-01-02 13:38:03 +0100

    ch01: make name argument optional

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Purpose: Say hello
 
 import argparse
 
 parser = argparse.ArgumentParser(description="Say hello")
-parser.add_argument("name", help="Name to greet")
+parser.add_argument(
+    "-n", "--name", metavar="name", default="World", help="Name to greet"
+)
 args = parser.parse_args()
 print("Hello, " + args.name + "!")
