ff354b40b978155dbeb5adfa354c9906b0f2e528
theFippo <schmid.jeffrey@gmail.com>
2023-01-02 11:37:15 +0100

    ch01: add positional argument and help doc

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # Purpose: Say hello
-print("Hello, World!")
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Say hello")
+parser.add_argument("name", help="Name to greet")
+args = parser.parse_args()
+print("Hello, " + args.name + "!")
