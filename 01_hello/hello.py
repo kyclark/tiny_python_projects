ca2b9c62c0e8d09e392d06818ea0f41eef762c85
theFippo <schmid.jeffrey@gmail.com>
2023-01-02 13:46:09 +0100

    ch01: separate getting arguments from main

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -4,12 +4,16 @@
 import argparse
 
 
-def main():
+def get_args():
     parser = argparse.ArgumentParser(description="Say hello")
     parser.add_argument(
         "-n", "--name", metavar="name", default="World", help="Name to greet"
     )
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
     print("Hello, " + args.name + "!")
 
 
